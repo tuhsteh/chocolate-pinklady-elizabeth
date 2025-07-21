332fa7f13edfe32dfd6dfcc78c9106ea34626342
tuhsteh <t.stear@gmail.com>
2025-07-20 21:10:19 -0500

    needed to ignore some paths in Prettier/ESLint; needed to ignore generated sources

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import stylistic from '@stylistic/eslint-plugin';
 
 export default [
   {
+    ignores: ['./src/generated'],
     plugins: {
       '@stylistic': stylistic,
     },
@@ -9,7 +10,7 @@ export default [
       '@stylistic/eol-last': ['error', 'always'],
       '@stylistic/indent': ['error', 2],
       '@stylistic/jsx-indent': 'error',
-      '@stylistic/semi': 'error',
+      '@stylistic/semi': 'error'
     },
-  },
+  }
 ];
